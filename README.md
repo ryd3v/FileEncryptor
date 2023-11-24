385fbf81b10760f1dfdff73ea25bd2391978498f
ryd3v <hello@ryd3v.com>
2023-11-24 15:09:56 -0500

    Release V2.0.0

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# File Encryptor Program
+# FileEncryptor Program
 
 ## Overview
 
-This File Encryption Program is a Python-based tool designed to securely encrypt and decrypt files using a
-password-derived key. It uses robust cryptographic methods, including the Argon2 algorithm for key derivation and Fernet
-symmetric encryption.
+This File Encryption Program is a Python-based tool designed to securely encrypt and decrypt large files using a
+password-derived key. It employs robust cryptographic methods, including the Argon2 algorithm for key derivation and
+AES-GCM for encryption, making it suitable for processing large files efficiently.
 
 ## Features
 
+- **AES-GCM Encryption**: Utilizes AES-GCM for secure and efficient encryption, suitable for large files.
 - **Password-Based Encryption**: Generates a secure encryption key based on a user-provided password.
-- **File Encryption and Decryption**: Allows users to encrypt files, adding a `.enc` extension, and decrypt them back to
-  their original state.
-- **Secure Key Management**: Securely stores the encryption key in an encrypted file, ensuring that only the password
-  holder can access it.
+- **File Encryption and Decryption**: Encrypts files with a `.enc` extension and decrypts them back to their original
+  state.
+- **Chunked File Processing**: Processes files in chunks to efficiently handle large files without excessive memory
+  usage.
+- **Progress Bar**: Displays a progress bar during encryption and decryption processes for better user feedback.
+- **Secure Key Management**: Securely stores the encryption key in an encrypted file, ensuring access only for the
+  password holder.
 
 ## Requirements
 
@@ -25,44 +29,42 @@ symmetric encryption.
 1. Ensure Python 3.x is installed on your system.
 2. Install required Python libraries:
    ```
-   pip install cryptography argon2-cffi
+   pip install -r requirements.txt
    ```
 
 ## Usage
 
-To use the File Encryption Program, navigate to the program's directory in the command line and use the following
-commands:
+Navigate to the program's directory in the command line and use the following commands:
 
 1. **Encrypt a File**:
    ```
-   python3 main.py encrypt <file_path>
+   python3 ./FileEncryptor encrypt <file_path>
    ```
-   This will encrypt the specified file and save it with a `.enc` extension.
+   This command encrypts the specified file and saves it with a `.enc` extension.
 
 2. **Decrypt a File**:
    ```
-   python3 main.py decrypt <file_path>.enc
+   python3 ./FileEncryptor decrypt <file_path>.enc
    ```
-   This will decrypt the specified `.enc` file back to its original format.
+   This decrypts the specified `.enc` file back to its original format.
 
 ### First Run
 
-- Upon first execution, the program will prompt you to enter a password to generate a secure encryption key. This key
-  will be used for all encryption and decryption processes.
+- The program prompts the user for a password to generate a secure encryption key upon first execution.
 
 ### Subsequent Runs
 
-- On subsequent runs, you'll need to enter the same password to decrypt the key file and access the
-  encryption/decryption functionality.
+- Enter the same password used during the first run to decrypt the key file and access the encryption/decryption
+  functionality.
 
 ## Security
 
-- The program uses `750000` iterations in the key derivation process for enhanced security.
-- The primary key is securely stored in an encrypted file, accessible only with the correct password.
+- Employs `750000` iterations in the key derivation process for enhanced security.
+- The master key is securely stored in an encrypted file, accessible only with the correct password.
 
 ## Disclaimer
 
-- This tool is for educational purposes and should be thoroughly tested before using it for sensitive data.
+- This tool is intended for educational purposes and should be thoroughly tested before using it for sensitive data.
 
 ## License
 
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import base64
 import getpass
 import os
 import sys
 
-from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from tqdm import tqdm
 
 
 def generate_key(password: str, salt: bytes = None) -> (bytes, bytes):
@@ -21,37 +21,50 @@ def generate_key(password: str, salt: bytes = None) -> (bytes, bytes):
         backend=default_backend()
     )
     key = kdf.derive(password.encode())
-    return base64.urlsafe_b64encode(key), salt
+    return key, salt
 
 
 def encrypt_key_file(key: bytes, password: str):
     generated_key, salt = generate_key(password)
-    fernet = Fernet(generated_key)
-    encrypted_key = fernet.encrypt(key)
+    aesgcm = AESGCM(generated_key)
+    nonce = os.urandom(12)
+
+    encrypted_key = aesgcm.encrypt(nonce, key, None)
     with open('key_file', 'wb') as file:
-        file.write(salt + encrypted_key)
+        file.write(salt + nonce + encrypted_key)
 
 
 def decrypt_key_file(password: str) -> bytes:
     with open('key_file', 'rb') as file:
         salt = file.read(16)
+        nonce = file.read(12)
         encrypted_key = file.read()
 
     key = generate_key(password, salt)[0]
-    fernet = Fernet(key)
-    return fernet.decrypt(encrypted_key)
+    aesgcm = AESGCM(key)
+    return aesgcm.decrypt(nonce, encrypted_key, None)
 
 
 def encrypt_file(file_path: str, key: bytes):
-    fernet = Fernet(key)
-    with open(file_path, 'rb') as file:
-        file_data = file.read()
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
 
-    encrypted_data = fernet.encrypt(file_data)
-
+    chunk_size = 1024 * 1024  # 1MB chunk size
     encrypted_file_path = file_path + ".enc"
-    with open(encrypted_file_path, 'wb') as file:
-        file.write(encrypted_data)
+
+    total_size = os.path.getsize(file_path)
+
+    with open(file_path, 'rb') as input_file:
+        with open(encrypted_file_path, 'wb') as output_file:
+            output_file.write(nonce)
+            with tqdm(total=total_size, unit='B', unit_scale=True, desc="Encrypting") as pbar:
+                while True:
+                    chunk = input_file.read(chunk_size)
+                    if len(chunk) == 0:
+                        break
+                    encrypted_chunk = aesgcm.encrypt(nonce, chunk, None)
+                    output_file.write(encrypted_chunk)
+                    pbar.update(len(chunk))
 
     print(f"File {file_path} has been encrypted to {encrypted_file_path}.")
 
@@ -61,17 +74,29 @@ def decrypt_file(file_path: str, key: bytes):
         print("The file does not have the correct '.enc' extension.")
         return
 
-    fernet = Fernet(key)
-    with open(file_path, 'rb') as file:
-        encrypted_data = file.read()
+    aesgcm = AESGCM(key)
+    tag_length = 16
 
-    decrypted_data = fernet.decrypt(encrypted_data)
-
+    chunk_size = 1024 * 1024
     decrypted_file_path = file_path.rsplit(".enc", 1)[0]
-    with open(decrypted_file_path, 'wb') as file:
-        file.write(decrypted_data)
 
-    print(f"File {file_path} has been decrypted to {decrypted_file_path}")
+    total_size = os.path.getsize(file_path)
+
+    with open(file_path, 'rb') as input_file:
+        nonce = input_file.read(12)
+        total_size -= len(nonce)
+
+        with open(decrypted_file_path, 'wb') as output_file:
+            with tqdm(total=total_size, unit='B', unit_scale=True, desc="Decrypting") as pbar:
+                while True:
+                    encrypted_chunk = input_file.read(chunk_size + tag_length)
+                    if len(encrypted_chunk) == 0:
+                        break
+                    decrypted_chunk = aesgcm.decrypt(nonce, encrypted_chunk, None)
+                    output_file.write(decrypted_chunk)
+                    pbar.update(len(encrypted_chunk) - tag_length)
+
+    print(f"File {file_path} has been decrypted to {decrypted_file_path}.")
 
 
 def main():
@@ -84,7 +109,7 @@ def main():
 
     if not os.path.exists('key_file'):
         password = getpass.getpass(prompt="Enter a password to generate your key: ")
-        master_key = Fernet.generate_key()
+        master_key, _ = generate_key(password)  # Generate a raw key suitable for AESGCM
         encrypt_key_file(master_key, password)
     else:
         password = getpass.getpass(prompt="Enter your password to decrypt the key: ")
